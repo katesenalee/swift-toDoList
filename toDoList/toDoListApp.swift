6485b8116a07da6230bbc9740b9d4ba7283f4da8
sena <43688294+katesenalee@users.noreply.github.com>
2021-09-13 17:09:13 +0800

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Sena Lee on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
