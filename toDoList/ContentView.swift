43ee19db9a0107cf7a19a29b66f900cbdec4f33e
sena <43688294+katesenalee@users.noreply.github.com>
2021-09-13 17:44:51 +0800

    made checkboxes and strikethrough for completed todos #paulrudd

--- toDoList/ContentView.swift
+++ toDoList/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var todos = [
-        ToDo(title: "history notes"),
+        ToDo(title: "history notes", isCompleted: true),
         ToDo(title: "english essay"),
         ToDo(title: "chem video")
     ]
@@ -17,7 +17,14 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(todos) { todo in
-                Text(todo.title)
+                HStack {
+                    Image(systemName: todo.isCompleted ? "checkmark.square" : "squareshape")
+                        .font(.system(size: 12))
+                        .foregroundColor(todo.isCompleted ? .gray : .black)
+                    Text(todo.title)
+                        .strikethrough(todo.isCompleted)
+                        .foregroundColor(todo.isCompleted ? .gray : .black)
+                }
             }
             .navigationTitle("To-Dos")
         }
